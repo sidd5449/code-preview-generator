48123016f969b13dfc8d32f8a5c463c74f17e32f
cout"vaibhav <85737835+vaibhav1663@users.noreply.github.com>
2022-06-12 15:59:13 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 function fn1(){
     var str = document.getElementById('text').value;
     
@@ -22,3 +20,27 @@ function capture(){
 		anchorTag.click();
     });
 }
+
+const main=document.querySelector('#main__editor') ;
+const btn1= document.getElementById('button1');
+const btn2= document.querySelector('#button2');
+const btn3= document.querySelector('#button3');
+const btn4= document.querySelector('#button4');
+const btn5= document.querySelector('#button5');
+
+btn1.addEventListener("click", () => {
+    main.style.background="linear-gradient(-45deg, rgb(63, 124, 246), rgb(209, 33, 142))";
+});
+btn2.addEventListener("click", () => {
+    main.style.background="linear-gradient(-45deg, #2E3192 , #1BFFFF)";
+});
+btn3.addEventListener("click", () => {
+    main.style.background="linear-gradient(-45deg, #D4145A ,#FBB03B)";
+});
+btn4.addEventListener("click", () => {
+    main.style.background="linear-gradient(-45deg,#FF5F6D, #FFC371)";
+});
+btn5.addEventListener("click", () => {
+    main.style.background="linear-gradient(-45deg, #35C3F3 0%, #8b9fe8 20%, #e681d8 39%, #ffa9a4 76%, #FED2CE 100%)";
+});
+  
